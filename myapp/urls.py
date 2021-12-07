b9b5f7e186e86c1e3ae14328499e1d8664e39893
bogdankozlovsky <bogdan.k@datarails.com>
2021-12-07 19:43:10 +0100

    add models

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path("hello/", views.hello, name="main-page"),
     path("add_rate/<int:rate>/<int:book_id>/", views.add_rate, name="add-rate"),
     path("order_book/<int:book_id>/", views.order_book, name="order-book"),
+    path("add_comment/<int:book_id>/", views.add_comment, name="add-comment"),
+    path("delete_comment/<int:comment_id>/", views.delete_comment, name="delete-comment"),
+    path("update_comment/<int:comment_id>/", views.update_comment, name="update-comment"),
+    path("my_account/", views.my_account, name="my-account"),
+    path("logout/", views.logout_view, name="logout"),
+    path("login/", views.login_view, name="login")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from myapp.models import Book, RateBookUser, OrderBookUser,Comment
-from django.db.models import Avg, Sum, Prefetch, Count
+from myapp.models import Book, RateBookUser, OrderBookUser, Comment
+from django.db.models import Avg, Sum, Prefetch, Count, F
+# HW: do CRUD for Books
+#     implement ability for like to comment
 
 
 def hello(request):
@@ -12,17 +16,76 @@ def hello(request):
     return render(request, "index.html", {"books": query_2})
 
 
+@login_required(login_url="login")
 def add_rate(request, rate, book_id):
-    if request.user.is_authenticated:
-        RateBookUser.objects.update_or_create(user_id=request.user.id, book_id=book_id, defaults={"rate": rate})
+    RateBookUser.objects.update_or_create(user_id=request.user.id, book_id=book_id, defaults={"rate": rate})
     return redirect("main-page")
 
 
+@login_required(login_url="login")
 def order_book(request, book_id):
-    if request.user.is_authenticated:
-        OrderBookUser.objects.create(
-            count=request.POST.get("count"),
-            user_id=request.user.id,
-            book_id=book_id
-        )
+    OrderBookUser.objects.create(
+        count=request.POST.get("count"),
+        user_id=request.user.id,
+        book_id=book_id
+    )
+    return redirect("main-page")
+
+
+@login_required(login_url="login")
+def add_comment(request, book_id):
+    Comment.objects.create(
+        user_id=request.user.id,
+        text=request.POST.get("comment"),
+        book_id=book_id
+    )
+    return redirect("main-page")
+
+
+@login_required(login_url="login")
+def delete_comment(request, comment_id):
+    query_set = Comment.objects.filter(id=comment_id)
+    if query_set.exists():
+        if query_set.first().user.id == request.user.id:
+            query_set.delete()
+    return redirect("main-page")
+
+
+@login_required(login_url="login")
+def update_comment(request, comment_id):
+    comment_query = Comment.objects.filter(id=comment_id)
+    if comment_query.exists():
+        if request.method == "GET":
+            return render(request, "comment_form.html", {"comment": comment_query.first()})
+        if request.method == "POST":
+            comment_query.update(text=request.POST.get("comment"))
+    return redirect("main-page")
+
+
+@login_required(login_url="login")
+def my_account(request):
+    ordered_book_query_set = OrderBookUser.objects.filter(user_id=request.user.id).select_related("book").order_by("book__title")
+    ordered_book_query_set = ordered_book_query_set.annotate(total_price=F("book__price") * F("count"))
+    ordered_book_query_set = ordered_book_query_set.only("count", "date", "book__title", "book__price")
+    tt_price = ordered_book_query_set.aggregate(tt_sum=Sum("total_price"))
+    context = {"ordered_book": ordered_book_query_set}
+    context.update(tt_price)
+    return render(request, "ordered_book.html", context)
+
+
+@login_required(login_url="login")
+def logout_view(request):
+    logout(request)
+    return redirect("main-page")
+
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, "login.html")
+    if request.method == "POST":
+        user = authenticate(request, username=request.POST.get("login"), password=request.POST.get("password"))
+        if user is not None:
+            login(request, user)
+        else:
+            return redirect("login")
     return redirect("main-page")
